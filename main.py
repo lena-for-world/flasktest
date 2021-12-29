c3076d47dc611fc5ca8a0add71d9af13c522eccf
lena-for-world <kimrlyunah@gmail.com>
2021-12-30 01:10:59 +0900

    push 4

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def pic() :
 if __name__ == '__main__' :
     # 실행할 host, port 파라미터로 넣기
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
 
 
 
